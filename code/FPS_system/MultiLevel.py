239cc4d3674b572980bac63e4eb20bb0940119d6
valdeezzee <jvalderrama91@knights.ucf.edu>
2018-01-18 01:06:52 -0500

    fixed code name

--- code/FPS_system/MultiLevel.py
+++ code/FPS_system/MultiLevel.py
@@ -1,8 +1,8 @@
 from ParkingLot import ParkingLot
 from Location import Location
 from ParkingSpot import ParkingSpot
-class MulitLevel(ParkingLot):
-	"""docstring for MulitLevel"""
+class MultiLevel(ParkingLot):
+	"""docstring for MultiLevel"""
 	def __init__(self, location, parkingSpots, w, l, h, f):
 		self.floors = f
 		ParkingLot.__init__(self, location, parkingSpots, w, l, h)
